5b5886c996a6bb525c3336ef78e95c562712fc2e
Oles Marola <oles_pw@ukr.net>
2019-12-03 10:09:50 +0200

    Updates

---
+++ precache-manifest.13f22e97efac0fe594d18c5ac4c62c8c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c29762e80eb2b6a5a5b9962ede815c",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "6ea624aa9b9697035ed0",
+    "url": "/cra-demo/static/js/2.a3e8d21e.chunk.js"
+  },
+  {
+    "revision": "67d5126a8d8830f515aa",
+    "url": "/cra-demo/static/js/main.44eefce0.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js"
+  "/cra-demo/precache-manifest.13f22e97efac0fe594d18c5ac4c62c8c.js"
 );
 
 self.addEventListener('message', (event) => {
